214e66be1fa4e1162c35991f4a59425c616538f3
anfal11 <anfal15-14303@diu.edu.bd>
2023-10-04 05:01:04 +0600

    left side nav created with dynamic data

--- src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const LeftSideNav = () => {
-    return (
-        <div className="mt-5">
-            <h1 className="text-2xl">left side</h1>
-        </div>
-    );
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("/categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+  return (
+    <div className="mt-5">
+      <h2 className="text-xl font-semibold text-[#403F3F] mb-5">
+        {" "}
+        All Category
+      </h2>
+
+      <div>
+        {categories.map((category) => (
+          <Link
+            to={`/category/${category.id}`}
+            key={category.id}
+            className="block mt-2 text-base text-[#706F6F] hover:text-black hover:bg-[#E7E7E7] p-3 text-center rounded-md"
+          >
+            {category.name}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default LeftSideNav;
