05c78d3c90c74b7ace061312f3d463eb2c208e8b
anfal11 <anfal15-14303@diu.edu.bd>
2023-10-04 04:44:18 +0600

    qZOne added

--- src/Components/QZone/QZone.jsx
+++ src/Components/QZone/QZone.jsx
@@ -1,19 +1,15 @@
-
+import qzone1 from "../../assets/qZone1.png";
+import qzone2 from "../../assets/qZone2.png";
+import qzone3 from "../../assets/qZone3.png";
 const QZone = () => {
     return (
         <div>
-            <div className='p-4'>
-        <h2 className="text-xl font-semibold mb-4"> Find Us On</h2>
+            <div className='p-4 bg-[#F3F3F3] mt-5'>
+        <h2 className="text-xl font-semibold mb-4"> Q-Zone</h2>
         <div>
-        <Link to='https://www.facebook.com/beastslayerfiend.12/' target='_blank' className='flex rounded-t-lg border p-4'>
-        <GrFacebookOption className='mr-2 bg-gray-200 rounded-full text-[#3B599C] text-2xl'></GrFacebookOption>
-        Facebook</Link>
-        <Link to='https://twitter.com/RatulAnfal' target='_blank' className='flex border p-4'>
-        <AiOutlineTwitter className='mr-2 bg-gray-200 rounded-full text-[#58A7DE] text-2xl'></AiOutlineTwitter>
-        Twitter</Link>
-        <Link to='https://www.instagram.com/anfal_ratul/' className='flex rounded-b-lg border p-4' target='_blank'>
-        <AiOutlineInstagram className='mr-2 bg-gray-200 rounded-full text-[#D82D7E] text-2xl'></AiOutlineInstagram>
-        Instagram</Link>
+            <img src={qzone1} alt="" />
+            <img src={qzone2} alt="" />
+            <img src={qzone3} alt="" />
         </div>
       </div>
         </div>
