a7a5f703b03f6170b154e2012a510e009eb2ed80
anfal11 <anfal15-14303@diu.edu.bd>
2023-10-04 10:43:27 +0600

    newsCard created and it's function has also been dynamically created

--- src/Pages/Home/NewsCard.jsx
+++ src/Pages/Home/NewsCard.jsx
@@ -1,44 +1,90 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import { CiBookmark } from "react-icons/ci";
+import { BsShare } from "react-icons/bs";
+import ReactStarsRating from 'react-awesome-stars-rating';
+import { BsEye } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 
+const NewsCard = ({ news }) => {
+  const {
+    _id,
+    title,
+    details,
+    author,
+    total_view,
+    rating,
+    image_url
+  } = news;
+  return (
+    <div>
+    <h1 className="text-xl font-semibold"> Hot News </h1>
+      <div className="border rounded-md my-8">
+        {/* head */}
 
+        <div className="flex justify-between items-center bg-[#F3F3F3] px-4 py-3">
+          {/* left */}
 
-const NewsCard = ({news}) => {
+          <div className="flex gap-4">
+            <img className="rounded-full w-10" src={author.img} alt="" />
 
-    const {_id, title, description, author,thumbnail_url, total_view, rating, others_info,} = news;
-    return (
-        <div>
             <div>
-  {/* head */}
-<div className="flex justify-between">
- {/* left */}
-    <div className="flex gap-4">
-      <img className="rounded-full w-10" src={author.img} alt=""/>
+              <h1 className="text-base font-semibold"> {author.name}</h1>
+              <p className="text-[#706F6F] font-normal text-sm">{author.published_date}</p>
+            </div>
+          </div>
 
-      <div>
-        <h1> {author.name}</h1>
-        <p>{author.published_date}</p>
-      </div>
-  </div>
-{/* right */}
-  <div className="flex gap-3">
-      <h1>a</h1>
-      <h2>b</h2>
-  </div>
-</div>
+          {/* right */}
 
-{/* body */}
-<div className="p-5 border-b-2">
-  <h1>Biden Pledges Nearly $3 Billion To Ukraine In Largest U.S. Military Aid Package Yet</h1>
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9hxIGIRPVvvpnSQjDGNI0undzKEHbVYvWe-7bvt9W4A&s" alt=""/>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum suscipit, sunt officiis inventore exercitationem eaque veniam ad velit corporis dolorum veritatis, fugit, tempora molestiae asperiores obcaecati? Adipisci ex voluptate iure.... read</p>
-</div>
-</div>
+          <div className="flex gap-3">
+            <CiBookmark className="text-xl"></CiBookmark>
+            <BsShare className="text-xl"></BsShare>
+          </div>
         </div>
-    );
+
+        {/* body */}
+        <div className="p-5">
+          <div className="border-b-2">
+          <h1 className="text-[#403F3F] text-xl font-bold mb-5">
+            {title}
+          </h1>
+          <img className="mb-8"
+            src={image_url}
+            alt=""
+          />
+          <p className="text-base font-normal mb-5">
+          {
+            details.length > 200 ? <span className="text-[#706F6F]">{details.slice(0, 200)}<Link to={`/news/${_id}`} className="text-[#FF8C47]">...read more</Link></span> : <span className="text-[#706F6F]">{details}</span>
+          }
+          </p>
+          </div>
+        </div>
+
+        {/* parent of rating, view */}
+<div className="flex justify-between px-4">
+    
+        {/* rating left*/}
+        <div className="flex items-center gap-4">
+            <ReactStarsRating
+                className="flex my-3"
+                value={rating.number}
+                isEdit={false}
+            />
+            <p className="text-[#706F6F] text-base font-medium">{rating.number}</p>
+        </div>
+
+        {/* view right*/}
+        <div className="flex items-center gap-4">
+            <BsEye className="text-xl"></BsEye>
+            <p className="text-[#706F6F] text-base font-medium">{total_view}</p>
+        </div>
+</div>
+      </div>
+    </div>
+  );
 };
 
 NewsCard.propTypes = {
-    news: PropTypes.object.isRequired
-}
+  news: PropTypes.object.isRequired,
+};
 export default NewsCard;
