f5f2e137b181a41dff56b9b3098e6949f00592cb
anfal11 <anfal15-14303@diu.edu.bd>
2023-10-05 06:24:16 +0600

    done

--- src/Pages/Career/Career.jsx
+++ src/Pages/Career/Career.jsx
@@ -1,9 +1,38 @@
+import BreakingNews from "../Home/BreakingNews";
+import Header from "../Shared/Header/Header";
+import Navbar from "../Shared/Navbar/Navbar";
+
 const Career = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <Header />
+      <BreakingNews />
+      <Navbar />
+      <div className="2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4">
+        <p className="font-normal text-sm leading-3 text-indigo-700 dark:text-indigo-500 hover:text-indigo-800 cursor-pointer pb-2">Career</p>
+        <div className="flex lg:flex-row flex-col lg:gap-8 sm:gap-10 gap-12">
+          <div className="w-full lg:w-6/12">
+            <h2 className="w-full font-bold lg:text-4xl text-3xl lg:leading-10 dark:text-white leading-9">We are here to make great design accessible and delightful for everyone</h2>
+            <p className="font-normal text-base leading-6 text-gray-600  mt-6">It is a long-established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum. In the first place, we have granted to God, and by this, our present charter confirmed for us and our heirs forever that the English Church shall be free and shall have her rights entire.</p>
+          </div>
+          <div className="w-full lg:w-6/12">
+            <img className="lg:block hidden w-full" src="https://i.ibb.co/RjNH7QB/Rectangle-122-1.png" alt="people discussing on board" />
+            <img className="lg:hidden sm:block hidden w-full" src="https://i.ibb.co/16fPqrg/Rectangle-122-2.png" alt="people discussing on board" />
+            <img className="sm:hidden block w-full" src="https://i.ibb.co/Jxhpxh6/Rectangle-122.png" alt="people discussing on board" />
+          </div>
         </div>
-    );
+
+        <div className="relative mt-24">
+          <div className="grid sm:grid-cols-3 grid-cols-2 sm:gap-8 gap-4">
+            <div className="z-20 w-12 h-12 bg-gray-800 rounded-full flex justify-center items-center">
+              <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/about-us-3-svg1.svg" alt="" />
+            </div>
+            {/* Add more grid items as needed */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Career;
